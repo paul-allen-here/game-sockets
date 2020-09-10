c2891714e5a819d78edd5e79cb2246450e2fa386
Wenomr <wenom998@gmail.com>
2020-09-10 15:22:51 +0300

    first commit

--- cardParser.js
+++ cardParser.js
@@ -8,8 +8,8 @@ const whiteCards = []
 const parseCards = () => {
     let raw = []
 
-    raw = fs.readFileSync('white.txt').toString().split("\n")
-    
+    raw = fs.readFileSync('./resources/white.txt').toString().split("\n")
+
     for (let data of raw) {
         if (data.length > 4) {
             let whiteCard = new WhiteCard(whiteCards.length + 1, data, null)
@@ -17,7 +17,7 @@ const parseCards = () => {
         }
     }
 
-    raw = fs.readFileSync('black.txt').toString().split("\n")
+    raw = fs.readFileSync('./resources/black.txt').toString().split("\n")
     for (let data of raw) {
         let blackCard = new BlackCard(blackCards.length + 1, data)
         blackCards.push(blackCard)
